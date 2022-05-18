d563a00ad374a0d22cb4764554c1b8d295e093e8
Lion8cake <69915693+Lion8cake@users.noreply.github.com>
2022-05-18 23:39:52 +1000

    more changes
    title
    more creamwood furniture
    new icon test (its getting an update)

---
+++ Tiles/Furniture/CreamwoodBookcase.cs
@@ -0,0 +1,37 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.Enums;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.DataStructures;
+
+namespace TheConfectionRebirth.Tiles.Furniture
+{
+	class CreamwoodBookcase : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileSolidTop[Type] = true;
+			Main.tileTable[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+			TileObjectData.newTile.Height = 4;
+			TileObjectData.newTile.Origin = new Point16(0, 3);
+			TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 18 };
+			TileObjectData.addTile(Type);
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Bookcase");
+			AdjTiles = new int[]{ 101 };
+			AddMapEntry(new Color(106, 65, 51), name);
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 64, 32, ModContent.ItemType<Items.Placeable.Furniture.CreamwoodBookcase>());
+		}
+	}
+}
