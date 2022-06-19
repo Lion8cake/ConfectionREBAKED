4082eb458ba4d17b9a7512ec0265d3bb0d6d77b5
Yreorel <68769795+Ouroel@users.noreply.github.com>
2022-06-19 11:43:47 +0800

    Update MiniRollerCookieSummonersShine.cs

--- Items/Weapons/Minions/RollerCookie/MiniRollerCookieSummonersShine.cs
+++ Items/Weapons/Minions/RollerCookie/MiniRollerCookieSummonersShine.cs
@@ -28,12 +28,12 @@ namespace TheConfectionRebirth.Items.Weapons.Minions.RollerCookie
             Projectile.height = 14;
             DrawOffsetX = -2;
             DrawOriginOffsetY = -2;
-            Projectile.extraUpdates = 1;
 
             Projectile.friendly = true;
             Projectile.usesLocalNPCImmunity = true;
             Projectile.localNPCHitCooldown = 60;
             Projectile.penetrate = -1;
+            Projectile.extraUpdates = 1;
 
             Projectile.timeLeft = 1200;
         }
@@ -46,6 +46,29 @@ namespace TheConfectionRebirth.Items.Weapons.Minions.RollerCookie
         {
             return false;
         }
+
+        int skipUpdate
+        {
+            get { return (int)Projectile.ai[1] / 3; }
+            set { Projectile.ai[1] = value * 3 + Projectile.ai[1] % 3; }
+        }
+        int rollDir
+        {
+            get
+            {
+                int rv = (int)Projectile.ai[1] % 3;
+                if (rv == 2)
+                    rv = -1;
+                return rv;
+            }
+            set
+            {
+                int val = value;
+                if (val == -1)
+                    val = 2;
+                Projectile.ai[1] += val - (Projectile.ai[1] % 3);
+            }
+        }
         public override void AI()
         {
             const float blocksPerRotation = 9;
@@ -54,33 +77,41 @@ namespace TheConfectionRebirth.Items.Weapons.Minions.RollerCookie
 
             RollerCookieSummonProj.Unadhere(Projectile, (int)Projectile.ai[0]);
 
-            if (Projectile.ai[1] == 0)
+            if (rollDir == 0)
             {
-                if (Projectile.velocity.Y == 0)
+                if (Projectile.velocity.Y == 0 || Projectile.ai[0] != 0)
                 {
-                    Projectile.ai[1] = Math.Sign(Projectile.velocity.X);
-                    if (Projectile.ai[1] == 0)
+                    rollDir = Math.Sign(Projectile.velocity.X);
+                    if (rollDir == 0)
                     {
-                        Projectile.ai[1] = 1;
+                        rollDir = 1;
                     }
                 }
             }
             else {
-                Projectile.velocity.X = Projectile.ai[1] * 4;
+                Projectile.velocity.X = rollDir * 4;
             }
 
             int flipDirection = Projectile.velocity.X > 0 ? 1 : -1;
             Projectile.rotation += Projectile.velocity.Length() * flipDirection * rotationPerBlock;
-
             if (Projectile.ai[0] == 0)
-                Projectile.velocity.Y += normalGravity;
+            {
+                if (skipUpdate > 0)
+                    Projectile.velocity.Y = Math.Max(2, Projectile.velocity.Y) + 0.3f;
+                else
+                    Projectile.velocity.Y += normalGravity;
+            }
             else
-                Projectile.velocity.Y = Math.Max(10, Projectile.velocity.Y) + 0.3f;
+                Projectile.velocity.Y = Math.Max(8, Projectile.velocity.Y) + 0.3f;
 
             RollerCookieSummonProj.Adhere(Projectile, (int)Projectile.ai[0]);
-
-            Projectile.ai[0] = RollerCookieSummonProj.SwitchAdhering(Projectile, (int)Projectile.ai[0]);
-
+            Projectile.ai[0] = RollerCookieSummonProj.SwitchAdhering(Projectile, (int)Projectile.ai[0], skipUpdate > 0);
+            if (Projectile.ai[0] != 0)
+                skipUpdate = 30;
+            if (skipUpdate > 0)
+            {
+                skipUpdate--;
+            }
             Lighting.AddLight(Projectile.Center, new Vector3(0.67f, 0.44f, 0.0f) * 0.7f);
             RollerCookieSummonProj.CreateDust(Projectile);
 
