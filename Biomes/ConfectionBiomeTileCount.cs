98333ea7ec1bf0958fa64a8ed9a7ffe992f9ecfd
Yreorel <68769795+Ouroel@users.noreply.github.com>
2022-06-20 23:15:17 +0800

    Update ConfectionBiomeTileCount.cs

--- Biomes/ConfectionBiomeTileCount.cs
+++ Biomes/ConfectionBiomeTileCount.cs
@@ -3,6 +3,8 @@ using Terraria;
 using Terraria.ModLoader;
 using TheConfectionRebirth.Tiles;
 
+using AltLibrary.Common.AltBiomes;
+
 namespace TheConfectionRebirth.Biomes
 {
     public class ConfectionBiomeTileCount : ModSystem
@@ -23,15 +25,26 @@ namespace TheConfectionRebirth.Biomes
             desertOverlaytileCount = tileCounts[ModContent.TileType<Creamsand>()]
                 + tileCounts[ModContent.TileType<HardenedCreamsand>()]
                 + tileCounts[ModContent.TileType<Creamsandstone>()];
+        }
+    }
 
-            confectionBlockCount = tileCounts[ModContent.TileType<CookieBlock>()]
-                + tileCounts[ModContent.TileType<Creamstone>()]
-                + tileCounts[ModContent.TileType<CreamGrass>()]
-                + tileCounts[ModContent.TileType<CreamBlock>()]
-                + tileCounts[ModContent.TileType<BlueIce>()]
-                + tileCounts[ModContent.TileType<Creamsand>()]
-                + tileCounts[ModContent.TileType<HardenedCreamsand>()]
-                + tileCounts[ModContent.TileType<Creamsandstone>()];
+    public class ConfectionTileCounter : ALBiomeTileCountModifier
+    {
+        public override int GetTileCount(ReadOnlySpan<int> TileCounts)
+        {
+            return TileCounts[ModContent.TileType<CookieBlock>()]
+                + TileCounts[ModContent.TileType<Creamstone>()]
+                + TileCounts[ModContent.TileType<CreamGrass>()]
+                + TileCounts[ModContent.TileType<CreamBlock>()]
+                + TileCounts[ModContent.TileType<BlueIce>()]
+                + TileCounts[ModContent.TileType<Creamsand>()]
+                + TileCounts[ModContent.TileType<HardenedCreamsand>()]
+                + TileCounts[ModContent.TileType<Creamsandstone>()];
+        }
+
+        public override void OnReceiveModifiedTileCount(int TileCount)
+        {
+            ModContent.GetInstance<ConfectionBiomeTileCount>().confectionBlockCount = TileCount;
         }
     }
 }
