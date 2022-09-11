196d89812d9696090cf614be6351a1f79f05c058
Yreorel <68769795+Ouroel@users.noreply.github.com>
2022-09-11 13:20:27 +0800

    Update GummyFishSummonBuff.cs

--- Items/Weapons/Minions/GummyFish/GummyFishSummonBuff.cs
+++ Items/Weapons/Minions/GummyFish/GummyFishSummonBuff.cs
@@ -3,28 +3,7 @@ using Terraria.ModLoader;
 
 namespace TheConfectionRebirth.Items.Weapons.Minions.GummyFish
 {
-	public class GummyFishSummonBuff : ModBuff
+	public class GummyFishSummonBuff : MinionBuffBaseClass<GummyFishSummonProj>
 	{
-		public override void SetStaticDefaults()
-		{
-			DisplayName.SetDefault("Flying Gummy Fish");
-			Description.SetDefault("Flying gummy fish will fly after your enemies.");
-			Main.buffNoTimeDisplay[Type] = true;
-			Main.buffNoSave[Type] = true;
-		}
-	
-		public override void Update(Player player, ref int buffIndex) {
-				ConfectionPlayer modPlayer = player.GetModPlayer<ConfectionPlayer>();
-				if (player.ownedProjectileCounts[ModContent.ProjectileType<GummyFishSummonProj>()] > 0) {
-					modPlayer.flyingGummyFish = true;
-				}
-				if (!modPlayer.flyingGummyFish) {
-					player.DelBuff(buffIndex);
-					buffIndex--;
-				}
-				else {
-					player.buffTime[buffIndex] = 18000;
-				}
-			}
 	}
 }
