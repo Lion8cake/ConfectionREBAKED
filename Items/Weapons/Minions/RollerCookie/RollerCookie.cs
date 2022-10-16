ecf9adb80db657d4f8540d185043699a1cfe711a
Yreorel <68769795+Ouroel@users.noreply.github.com>
2022-10-16 19:18:58 +0800

    Update RollerCookie.cs

--- Items/Weapons/Minions/RollerCookie/RollerCookie.cs
+++ Items/Weapons/Minions/RollerCookie/RollerCookie.cs
@@ -22,7 +22,7 @@ namespace TheConfectionRebirth.Items.Weapons.Minions.RollerCookie
     {
         public override int Damage => 45;
         public override float Knockback => 3;
-        public override int Value => Item.buyPrice(0, 2, 0, 0);
+        public override int Value => Item.buyPrice(0, 4, 0, 0);
         public override void AddRecipes()
         {
             CreateRecipe()
