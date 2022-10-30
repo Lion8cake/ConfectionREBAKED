780a2739e6b8dca6920383b062b09225a7745eb3
BasicallyIAmFox <65787578+BasicallyIAmFox@users.noreply.github.com>
2022-10-31 01:46:55 +0500

    Update HeavenGift.cs

--- Items/Archived/HeavenGift.cs
+++ Items/Archived/HeavenGift.cs
@@ -8,13 +8,6 @@ namespace TheConfectionRebirth.Items.Archived
 {
 	public class HeavenGift : ModItem
 	{
-		public override void SetStaticDefaults() {
-			DisplayName.SetDefault("Heaven's Gift");
-			Tooltip.SetDefault("An old Legacy item ment for the 1.3 version of the confection."
-				+ "\nThank you to all who have supported the confection in the past, thank you just thanks!"
-				+ "\nSell this to any npc to get a platinum coin.");
-		}
-
 		public override void SetDefaults() {
 			Item.width = 20;
 			Item.height = 20;
