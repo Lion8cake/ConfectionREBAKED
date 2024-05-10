ccbab685ae8f37cf2eac0eea2bc152ff5fb08cff
Lion8cake <Lion8cake@hotmail.com>
2024-05-11 01:28:34 +1000

    Almost finished PlantCheck ILedit

--- TheConfectionRebirth.cs
+++ TheConfectionRebirth.cs
@@ -35,7 +35,6 @@ namespace TheConfectionRebirth
 			On_Player.DoesPickTargetTransformOnKill += PickaxeKillTile;
 			IL_Liquid.DelWater += BurnGrass;
 			IL_WorldGen.PlantCheck += PlantTileFrameIL;
-			//On_WorldGen.PlantCheck += PlantTileFrameDetour;
 		}
 
 		public override void Unload() {
@@ -43,220 +42,68 @@ namespace TheConfectionRebirth
 			On_Player.DoesPickTargetTransformOnKill -= PickaxeKillTile;
 			IL_Liquid.DelWater -= BurnGrass;
 			IL_WorldGen.PlantCheck -= PlantTileFrameIL;
-			//On_WorldGen.PlantCheck -= PlantTileFrameDetour;
-		}
-
-		private void PlantTileFrameDetour(On_WorldGen.orig_PlantCheck orig, int x, int y) {
-			//Orig-less detour, it gives an example of what the IL edit is supposed to do
-			x = Utils.Clamp(x, 1, Main.maxTilesX - 2);
-			y = Utils.Clamp(y, 1, Main.maxTilesY - 2);
-			for (int i = x - 1; i <= x + 1; i++) {
-				for (int j = y - 1; j <= y + 1; j++) {
-					if (Main.tile[i, j] == null) {
-						return;
-					}
-				}
-			}
-			int num = -1;
-			Tile tile = Main.tile[x, y];
-			int num2 = tile.TileType;
-			_ = Main.maxTilesX;
-			if (y + 1 >= Main.maxTilesY) {
-				num = num2;
-			}
-			if (x - 1 >= 0 && Main.tile[x - 1, y] != null) {
-				tile = Main.tile[x - 1, y];
-				if (tile.HasUnactuatedTile) {
-					tile = Main.tile[x - 1, y];
-					_ = ref tile.TileType;
-				}
-			}
-			if (x + 1 < Main.maxTilesX && Main.tile[x + 1, y] != null) {
-				tile = Main.tile[x + 1, y];
-				if (tile.HasUnactuatedTile) {
-					tile = Main.tile[x + 1, y];
-					_ = ref tile.TileType;
-				}
-			}
-			if (y - 1 >= 0 && Main.tile[x, y - 1] != null) {
-				tile = Main.tile[x, y - 1];
-				if (tile.HasUnactuatedTile) {
-					tile = Main.tile[x, y - 1];
-					_ = ref tile.TileType;
-				}
-			}
-			if (y + 1 < Main.maxTilesY && Main.tile[x, y + 1] != null) {
-				tile = Main.tile[x, y + 1];
-				if (tile.HasUnactuatedTile) {
-					tile = Main.tile[x, y + 1];
-					if (!tile.IsHalfBlock) {
-						tile = Main.tile[x, y + 1];
-						if (tile.Slope == 0) {
-							tile = Main.tile[x, y + 1];
-							num = tile.TileType;
-						}
-					}
-				}
-			}
-			if (x - 1 >= 0 && y - 1 >= 0 && Main.tile[x - 1, y - 1] != null) {
-				tile = Main.tile[x - 1, y - 1];
-				if (tile.HasUnactuatedTile) {
-					tile = Main.tile[x - 1, y - 1];
-					_ = ref tile.TileType;
-				}
-			}
-			if (x + 1 < Main.maxTilesX && y - 1 >= 0 && Main.tile[x + 1, y - 1] != null) {
-				tile = Main.tile[x + 1, y - 1];
-				if (tile.HasUnactuatedTile) {
-					tile = Main.tile[x + 1, y - 1];
-					_ = ref tile.TileType;
-				}
-			}
-			if (x - 1 >= 0 && y + 1 < Main.maxTilesY && Main.tile[x - 1, y + 1] != null) {
-				tile = Main.tile[x - 1, y + 1];
-				if (tile.HasUnactuatedTile) {
-					tile = Main.tile[x - 1, y + 1];
-					_ = ref tile.TileType;
-				}
-			}
-			if (x + 1 < Main.maxTilesX && y + 1 < Main.maxTilesY && Main.tile[x + 1, y + 1] != null) {
-				tile = Main.tile[x + 1, y + 1];
-				if (tile.HasUnactuatedTile) {
-					tile = Main.tile[x + 1, y + 1];
-					_ = ref tile.TileType;
-				}
-			}
-			//if (num2 == confection_foliage && num != CreamGrass)
-			//	goto IL_0510;
-			if ((num2 != 3 || num == 2 || num == 477 || num == 78 || num == 380 || num == 579) && (num2 != 73 || num == 2 || num == 477 || num == 78 || num == 380 || num == 579) && (num2 != 24 || num == 23 || num == 661) && (num2 != 61 || num == 60) && (num2 != 74 || num == 60) && (num2 != 71 || num == 70) && (num2 != 110 || num == 109 || num == 492) && (num2 != 113 || num == 109 || num == 492) && (num2 != 201 || num == 199 || num == 662) && (num2 != 637 || num == 633) && (num2 != ModContent.TileType<CreamGrass_Foliage>() || num == ModContent.TileType<CreamGrass>() || num == ModContent.TileType<CreamGrassMowed>())) {
-				return;
-			}
-			bool flag = false;
-			if (num2 == 3 || num2 == 110 || num2 == 24) {
-				tile = Main.tile[x, y];
-				flag = tile.TileFrameX == 144;
-			}
-			if (num2 == 201) {
-				tile = Main.tile[x, y];
-				flag = tile.TileFrameX == 270;
-			}
-			if ((num2 == 3 || num2 == 73) && num != 2 && num != 477) {
-				tile = Main.tile[x, y];
-				if (tile.TileFrameX >= 162) {
-					tile = Main.tile[x, y];
-					tile.TileFrameX = 126;
-				}
-			}
-			if (num2 == 74 && num != 60) {
-				tile = Main.tile[x, y];
-				if (tile.TileFrameX >= 162) {
-					tile = Main.tile[x, y];
-					tile.TileFrameX = 126;
-				}
-			}
-			switch (num) {
-				case 23:
-				case 661:
-					num2 = 24;
-					tile = Main.tile[x, y];
-					if (tile.TileFrameX >= 162) {
-						tile = Main.tile[x, y];
-						tile.TileFrameX = 126;
-					}
-					break;
-				case 2:
-				case 477:
-					num2 = ((num2 != 113) ? 3 : 73);
-					break;
-				case 109:
-				case 492:
-					num2 = ((num2 != 73) ? 110 : 113);
-					break;
-				case 199:
-				case 662:
-					num2 = 201;
-					break;
-				case 60:
-					num2 = 61;
-					while (true) {
-						tile = Main.tile[x, y];
-						if (tile.TileFrameX > 126) {
-							tile = Main.tile[x, y];
-							tile.TileFrameX -= 126;
-							continue;
-						}
-						break;
-					}
-					break;
-				case 70:
-					num2 = 71;
-					while (true) {
-						tile = Main.tile[x, y];
-						if (tile.TileFrameX <= 72) {
-							break;
-						}
-						tile = Main.tile[x, y];
-						tile.TileFrameX -= 72;
-					}
-					break;
-			}
-			int num3 = num2;
-			if (num == ModContent.TileType<CreamGrass>() || num == ModContent.TileType<CreamGrassMowed>()) {
-				num2 = ((num2 != 73) ? ModContent.TileType<CreamGrass_Foliage>() : 113);
-			}
-			tile = Main.tile[x, y];
-			if (num3 != tile.TileType) {
-				tile = Main.tile[x, y];
-				tile.TileType = (ushort)num2;
-				if (flag) {
-					tile = Main.tile[x, y];
-					tile.TileFrameX = 144;
-					if (num2 == 201) {
-						tile = Main.tile[x, y];
-						tile.TileFrameX = 270;
-					}
-				}
-			}
-			else {
-				WorldGen.KillTile(x, y);
-			}
 		}
 
 		private void PlantTileFrameIL(ILContext il) {
 			ILCursor c = new(il);
-			ILLabel IL_0433 = null;
+			ILLabel IL_0433 = c.DefineLabel();
 			if (!c.TryGotoNext(
-				MoveType.After,
-				i => i.MatchLdloc0(),
-				i => i.MatchLdcI4(662),
-				i => i.MatchBneUn(out IL_0433)
+				MoveType.Before,
+				i => i.MatchLdcI4(0),
+				i => i.MatchStloc2(),
+				i => i.MatchLdloc1(),
+				i => i.MatchLdcI4(3),
+				i => i.MatchBeq(out _)
 				)) {
-				ModContent.GetInstance<TheConfectionRebirth>().Logger.Debug("The Confection REBAKED: Plant Check massive if statement instructions was not found");
+				ModContent.GetInstance<TheConfectionRebirth>().Logger.Debug("Plant Check's main tile framing could not be found");
 				return;
 			}
-			if (IL_0433 == null)
+			IL_0433.Target = c.Next;
+			if (!c.TryGotoPrev(
+				MoveType.Before,
+				i => i.MatchLdloca(5),
+				i => i.MatchCall<Tile>("get_type"),
+				i => i.MatchPop(),
+				i => i.MatchLdloc1(),
+				i => i.MatchLdcI4(3),
+				i => i.MatchBneUn(out _)
+				)) {
+				ModContent.GetInstance<TheConfectionRebirth>().Logger.Debug("Plant Check's massive if statement instructions could not found");
 				return;
+			}
+			if (!c.TryGotoPrev(
+				MoveType.Before,
+				i => i.MatchLdloca(5),
+				i => i.MatchCall<Tile>("nactive"),
+				i => i.MatchBrfalse(out _)
+				)) {
+				ModContent.GetInstance<TheConfectionRebirth>().Logger.Debug("Plant Check's before massive if statement could not be found");
+				return;
+			}
+			if (IL_0433 == null) {
+				ModContent.GetInstance<TheConfectionRebirth>().Logger.Debug("Plant Check's main tile framing IlLable could not be found");
+				return;
+			}
 			c.EmitLdloc1(); //num2
 			c.EmitLdloc0(); //num
 			c.EmitDelegate((int num2, int num) => {
-				return (num2 != ModContent.TileType<CreamGrass_Foliage>() || num == ModContent.TileType<CreamGrass>() || num == ModContent.TileType<CreamGrassMowed>());
+				return (!(num2 != ModContent.TileType<CreamGrass_Foliage>() || num == ModContent.TileType<CreamGrass>() || num == ModContent.TileType<CreamGrassMowed>()));
 			});
 			c.EmitBrtrue(IL_0433);
 			
 			c.GotoNext(
-				MoveType.Before,
-				i => i.MatchLdloc2(),
-				i => i.MatchBrfalse(out _)
+				MoveType.After,
+				i => i.MatchLdcI4(0),
+				i => i.MatchStloc2()
 				);
 			c.EmitLdloc0(); //num
 			c.EmitLdloca(1); //num2
 			c.EmitDelegate((int num, ref int num2) => {
 				if (num == ModContent.TileType<CreamGrass>() || num == ModContent.TileType<CreamGrassMowed>()) 
 				{
-					num2 = ((num2 != 73) ? ModContent.TileType<CreamGrass_Foliage>() : 113);
+					num2 = ModContent.TileType<CreamGrass_Foliage>();
 				}
 			});
-			MonoModHooks.DumpIL(ModContent.GetInstance<TheConfectionRebirth>(), il);
 		}
 
 		private void BurnGrass(ILContext il) {
